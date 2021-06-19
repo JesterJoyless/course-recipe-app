e9094f9bfac86964f86023a78495b6968acf9b03
Saad Anwari <thepalerideris@gmail.com>
2021-06-19 16:10:20 +0500

    Application Initial Structure

---
+++ src/app/headers/headers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-headers',
+  templateUrl: './headers.componenet.html',
+  styleUrls:['./headers.component.css']
+})
+export class HeadersComponent{
+
+}
